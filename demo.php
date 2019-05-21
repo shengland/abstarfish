90b0f859a8befaee1fe3eed51730c67a6a99ca29
shengland <2770518845@qq.com>
2019-05-21 13:46:53 +0800

    第一个版本

---
+++ demo.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Summary: demo.php
+ *
+ * @author:    shengaland
+ * @Date  :    2019/5/21---13:43
+ */
+require_once './vendor/autoload.php';
+use Abstarfish\Starfish\Tools;
+
+Tools::hello();
---
+++ src/Abstarfish/Starfish/Tools.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Summary: Tools.php
+ *
+ * @author:    shengaland
+ * @Date  :    2019/5/21---11:17
+ */
+namespace Abstarfish\Starfish;
+
+class Tools
+{
+    public function __construct() {
+    }
+
+    public static function hello(){
+        echo 'hello world';
+    }
+}
